60580929bd3b95842a6ffe1d8f9f256c47afb8f0
Администратор <@WIN-TASPQIGHUC1>
2024-01-24 18:52:51 +0300

    Добавьте файлы проекта.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Matrix
+    {
+        readonly int matrixDim; // размерность
+
+        // исходные матрицы и произведение
+        public readonly int[,] matrixA;
+        public readonly int[,] matrixB;
+        public int[,] matrixAB;
+        public TimeSpan Wtime {get;set;}
+        static Random rnd = new Random();
+        DateTime startTime;
+
+        // Конструктор
+        public Matrix(int d)
+        {
+            matrixDim = d;
+            matrixA = new int[d, d];
+            matrixB = new int[d, d];
+            matrixAB = new int[d, d];
+
+            // Создаём две матрицы и заполняем случайными числами 1..10
+            for (int i = 0; i < d; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    matrixA[i, j] = rnd.Next(0, 11);
+                    matrixB[i, j] = rnd.Next(0, 11);
+                }
+            }
+        }
+
+        // ****************************************
+        // * Распараллеливание операции умножения *
+        // ****************************************
+        public int[,] ParallelMult(int[,] matrixA, int[,] matrixB)
+        {
+            startTime = DateTime.Now; // Время начала расчёта
+
+            Parallel.For(0, matrixDim, iline => {
+                Parallel.For(0, matrixDim, i => MultMatrix(matrixAB, i, iline, matrixDim));
+            });
+
+            Wtime = DateTime.Now - startTime; // Прошедшее время
+            return matrixAB;
+        }
+
+        // Умножение двух квадратных матриц
+        private void MultMatrix(int[,] result, int i, int line, int len)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                result[line, i] += matrixA[line, j] * matrixB[j, i];
+            }
+        }
+
+        // Выходные данные
+        public void PrintMatrix(int[,] matrix, string name)
+        {
+            Console.WriteLine($"{name}\n");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Размерность квадратной матрицы: ");
+            int.TryParse(Console.ReadLine(), out int dim);
+            if (dim > 0)
+            {
+                Matrix mat = new Matrix(dim);
+                int[,] matrixResult = mat.ParallelMult(mat.matrixA, mat.matrixB);
+
+                mat.PrintMatrix(mat.matrixA, "\nМатрица A");
+                mat.PrintMatrix(mat.matrixB, "\nМатрица B");
+                mat.PrintMatrix(matrixResult, "\nМатрица A * B");
+
+                Console.WriteLine($"\nПрошло реального времени: {mat.Wtime}");
+                Console.WriteLine();
+                Console.WriteLine("Нажмите любую клавишу для завершения...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Размерность должна быть неотрицательным числом");
+                Console.WriteLine("Нажмите любую клавишу для завершения...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
